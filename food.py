f8f9835ea6c14713829cf1c0554f719da18912dd
Carson Bennett <bennec4222@clarkstate.edu>
2026-05-05 19:43:31 -0400

    Update food.py

--- food.py
+++ food.py
@@ -14,3 +14,20 @@
 #Your entered foods are:
 #pizza, beef jerkey, rice triangles, steamed chinese bun, fried chicken 
 #You entered a total of 62 characters
+
+foods = []
+
+for i in range(5):
+    food = input("Enter a food: ")
+    foods.append(food)
+
+print()  
+
+print("Your entered foods are:")
+print(", ".join(foods))
+
+total_chars = 0
+for food in foods:
+    total_chars += len(food)
+
+print(f"You entered a total of {total_chars} characters")
